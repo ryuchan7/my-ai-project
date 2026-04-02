2d9a366effe645373f4ff52332880281d9e49ac8
ryuchan7 <irfxnhxckxr7@gmail.com>
2026-04-02 16:18:01 +0800

    Updated main.py to the latest version

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('Hello Data Science')
+# UniMAP Data Science - Basic Calculator
+def add(x, y): return x + y
+def sub(x, y): return x - y
+
+print("--- Calculator Ready ---")
+val1 = float(input("First number: "))
+val2 = float(input("Second number: "))
+
+print(f"Result of addition: {add(val1, val2)}")
